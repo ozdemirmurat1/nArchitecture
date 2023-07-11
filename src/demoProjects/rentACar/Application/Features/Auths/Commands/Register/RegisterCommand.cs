686d27bae49be7357319e53aa206486568691b12
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-11 14:04:10 +0300

    AddAuthManager

--- src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Application.Services.Repositories;
 using Core.Security.Dtos;
 using MediatR;
 using System;
@@ -9,9 +11,27 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Auths.Commands.Register
 {
+    // RegisteredDto api den alacağımız dönüş
     public class RegisterCommand:IRequest<RegisteredDto>
     {
         public UserForRegisterDto UserForRegisterDto { get; set; }
         public string IpAddress { get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisteredDto>
+        {
+            private readonly AuthBusinessRules _authBusinessRules;
+            private readonly IUserRepository _userRepository;
+
+            public RegisterCommandHandler(AuthBusinessRules authBusinessRules, IUserRepository userRepository)
+            {
+                _authBusinessRules = authBusinessRules;
+                _userRepository = userRepository;
+            }
+
+            public Task<RegisteredDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
