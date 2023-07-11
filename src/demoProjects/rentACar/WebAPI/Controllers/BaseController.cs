e4eccd58c94d7a9faf8ec55f673c1a10effa329e
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-11 14:31:55 +0300

    AddAuthController

--- src/demoProjects/rentACar/WebAPI/Controllers/BaseController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BaseController.cs
@@ -8,5 +8,12 @@ namespace WebAPI.Controllers
         // ?? sol taraf null değilse değeri döndür
         protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         private IMediator? _mediator;
+
+        protected string? GetIpAddress()
+        {
+            // httpContext için IOC tarafında tanımlama yapmak lazım
+            ​if (Request.Headers.ContainsKey("X-Forwarded-For")) return Request.Headers["X-Forwarded-For"];
+            return HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+        }
     }
 }
