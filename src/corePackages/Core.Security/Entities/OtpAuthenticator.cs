bcbfb7f577fb36cdfc398752c3f1ae49124c89a5
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-13 17:09:44 +0300

    Commit

--- src/corePackages/Core.Security/Entities/OtpAuthenticator.cs
+++ src/corePackages/Core.Security/Entities/OtpAuthenticator.cs
@@ -2,21 +2,27 @@
 
 namespace Core.Security.Entities;
 
-public class OtpAuthenticator : Entity
+public class OtpAuthenticator : Entity<int>
 {
     public int UserId { get; set; }
     public byte[] SecretKey { get; set; }
     public bool IsVerified { get; set; }
 
-    public virtual User User { get; set; }
+    public virtual User User { get; set; } = null!;
 
     public OtpAuthenticator()
     {
+        SecretKey=Array.Empty<byte>();
+    }
+    public OtpAuthenticator(int userId, byte[] secretKey,bool isVerified)
+    {
+        UserId = userId;
+        SecretKey = secretKey;
+        IsVerified = isVerified;
     }
 
-    public OtpAuthenticator(int id, int userId, byte[] secretKey, bool isVerified) : this()
+    public OtpAuthenticator(int id, int userId, byte[] secretKey, bool isVerified) : base(id)
     {
-        Id = id;
         UserId = userId;
         SecretKey = secretKey;
         IsVerified = isVerified;
--- src/corePackages/Core.Security/Entities/UserOperationClaim.cs
+++ src/corePackages/Core.Security/Entities/UserOperationClaim.cs
@@ -2,16 +2,18 @@
 
 namespace Core.Security.Entities;
 
-public class UserOperationClaim : Entity
+public class UserOperationClaim : Entity<int>
 {
     public int UserId { get; set; }
     public int OperationClaimId { get; set; }
 
-    public virtual User User { get; set; }
-    public virtual OperationClaim OperationClaim { get; set; }
+    public virtual User User { get; set; } = null!;
+    public virtual OperationClaim OperationClaim { get; set; } = null!;
 
-    public UserOperationClaim()
+    public UserOperationClaim(int userId,int operationClaimId)
     {
+        UserId = userId;
+        OperationClaimId = operationClaimId;
     }
 
     public UserOperationClaim(int id, int userId, int operationClaimId) : base(id)
