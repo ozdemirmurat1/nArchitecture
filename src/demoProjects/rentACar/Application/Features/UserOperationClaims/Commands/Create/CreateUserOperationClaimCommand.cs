637d6ef57c5482fcb79cdddfd24dde028460eed6
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-16 23:45:41 +0300

    Commit

---
+++ src/demoProjects/rentACar/Application/Features/UserOperationClaims/Commands/Create/CreateUserOperationClaimCommand.cs
@@ -0,0 +1,51 @@
+﻿using Core.Application.Pipelines.Authorization;
+using MediatR;
+using static Application.Features.UserOperationClaims.Constants.UserOperationClaimsOperationClaims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Repositories;
+using AutoMapper;
+using Application.Features.UserOperationClaims.Rules;
+using Core.Security.Entities;
+
+namespace Application.Features.UserOperationClaims.Commands.Create
+{
+    public class CreateUserOperationClaimCommand : IRequest<CreatedUserOperationClaimResponse>, ISecuredRequest
+    {
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+        public string[] Roles => new[] { Admin, Write, Add };
+
+        public class CreateUserOperationClaimCommandHandler : IRequestHandler<CreateUserOperationClaimCommand, CreatedUserOperationClaimResponse>
+        {
+            private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+            private readonly IMapper _mapper;
+            private readonly UserOperationClaimBusinessRules _userOperationClaimBusinessRoles;
+
+            public CreateUserOperationClaimCommandHandler(IUserOperationClaimRepository userOperationClaimRepository, IMapper mapper, UserOperationClaimBusinessRules userOperationClaimBusinessRoles)
+            {
+                _userOperationClaimRepository = userOperationClaimRepository;
+                _mapper = mapper;
+                _userOperationClaimBusinessRoles = userOperationClaimBusinessRoles;
+            }
+
+            public async Task<CreatedUserOperationClaimResponse> Handle(CreateUserOperationClaimCommand request, CancellationToken cancellationToken)
+            {
+                await _userOperationClaimBusinessRoles.UserShouldNotHasOperationClaimAlreadyWhenInsert(
+                    request.UserId,
+                    request.OperationClaimId
+                    );
+
+                UserOperationClaim mappedUserOperationClaim = _mapper.Map<UserOperationClaim>(request);
+                UserOperationClaim createdUserOperationClaim=await _userOperationClaimRepository.AddAsync(mappedUserOperationClaim);
+
+                CreatedUserOperationClaimResponse createdUserOperationClaimDto=_mapper.Map<CreatedUserOperationClaimResponse>(createdUserOperationClaim);
+
+                return createdUserOperationClaimDto;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/UserOperationClaims/Commands/Create/CreatedUserOperationClaimResponse.cs
@@ -0,0 +1,16 @@
+﻿using Core.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Commands.Create
+{
+    public class CreatedUserOperationClaimResponse:IResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
