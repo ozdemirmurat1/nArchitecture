842037cce5ddf7c9f0a445102a67289fdd6fc221
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-17 11:33:59 +0300

    Commit

---
+++ src/demoProjects/rentACar/Application/Features/UserOperationClaims/Queries/GetList/GetListUserOperationClaimListItemDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Queries.GetList
+{
+    public class GetListUserOperationClaimListItemDto:IDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/UserOperationClaims/Queries/GetList/GetListUserOperationClaimQuery.cs
@@ -0,0 +1,21 @@
+﻿using Core.Application.Requests;
+using Core.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Queries.GetList
+{
+    public class GetListUserOperationClaimQuery:IRequest<GetListResponse<GetListUserOperationClaimListItemDto>>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public GetListUserOperationClaimQuery()
+        {
+            PageRequest = new PageRequest() { PageIndex=0,PageSize=10};
+        }
+    }
+}
