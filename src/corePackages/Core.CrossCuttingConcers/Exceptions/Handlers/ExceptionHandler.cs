84212f1ee201dd8f96aebe5b85f1f581c169aaa1
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-19 15:53:09 +0300

    Commit

---
+++ src/corePackages/Core.CrossCuttingConcers/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers
+{
+    public abstract class ExceptionHandler
+    {
+        public Task HandleExceptionAsync(Exception exception) =>
+            exception switch
+            {
+
+            };
+    }
+}
---
+++ src/corePackages/Core.CrossCuttingConcers/Exceptions/Types/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types
+{
+    public class NotFoundException:Exception
+    {
+        public NotFoundException() { }
+
+        public NotFoundException(SerializationInfo info,StreamingContext context):base(info,context) { }
+
+        public NotFoundException (string? message) : base(message) { }
+
+        public NotFoundException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
