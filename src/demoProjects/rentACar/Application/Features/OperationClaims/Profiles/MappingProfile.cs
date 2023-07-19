6e5c1de7aed90159ed22f39a4e6bf43761655061
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-19 12:59:07 +0300

    Commit

---
+++ src/demoProjects/rentACar/Application/Features/OperationClaims/Profiles/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.OperationClaims.Commands.Create;
+using Application.Features.OperationClaims.Commands.Delete;
+using Application.Features.OperationClaims.Commands.Update;
+using Application.Features.UserOperationClaims.Queries.GetById;
+using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
+using Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.OperationClaims.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserOperationClaim, CreateUserOperationClaimCommand>().ReverseMap();
+            CreateMap<UserOperationClaim, CreatedUserOperationClaimResponse>().ReverseMap();
+            CreateMap<UserOperationClaim, UpdateUserOperationClaimCommand>().ReverseMap();
+            CreateMap<UserOperationClaim, UpdatedUserOperationClaimResponse>().ReverseMap();
+            CreateMap<UserOperationClaim, DeleteUserOperationClaimCommand>().ReverseMap();
+            CreateMap<UserOperationClaim, DeletedUserOperationClaimResponse>().ReverseMap();
+            
+            
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/OperationClaims/Queries/GetList/GetListUserOperationClaimQuery.cs
@@ -0,0 +1,26 @@
+﻿using Core.Application.Requests;
+using Core.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.OperationClaims.Queries.GetList
+{
+    public class GetListUserOperationClaimQuery:IRequest<GetListResponse<GetListUserOperationClaimListItemDto>>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public GetListUserOperationClaimQuery()
+        {
+            PageRequest=new PageRequest { PageIndex=0,PageSize=10 };
+        }
+
+        public GetListUserOperationClaimQuery(PageRequest pageRequest)
+        {
+            PageRequest = pageRequest;
+        }
+    }
+}
