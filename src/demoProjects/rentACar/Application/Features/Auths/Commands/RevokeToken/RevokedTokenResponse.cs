17891929535c37af76f87b0e160d850508b34f68
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-25 14:56:45 +0300

    Commit

---
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/RevokeToken/RevokedTokenResponse.cs
@@ -0,0 +1,26 @@
+﻿using Core.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.RevokeToken
+{
+    public class RevokedTokenResponse:IResponse
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+
+        public RevokedTokenResponse()
+        {
+            Token = string.Empty;
+        }
+
+        public RevokedTokenResponse(int id,string token)
+        {
+            Id = id;
+            Token = token;
+        }
+    }
+}
