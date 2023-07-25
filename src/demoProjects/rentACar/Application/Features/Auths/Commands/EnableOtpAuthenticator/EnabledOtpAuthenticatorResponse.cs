43fb3be08e5e445b1fb4efdae14c97edda0da1f9
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-25 13:27:30 +0300

    Commit

---
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/EnableOtpAuthenticator/EnabledOtpAuthenticatorResponse.cs
@@ -0,0 +1,24 @@
+﻿using Core.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.EnableOtpAuthenticator
+{
+    public class EnabledOtpAuthenticatorResponse:IResponse
+    {
+        public string SecretKey { get; set; }
+
+        public EnabledOtpAuthenticatorResponse()
+        {
+            SecretKey=string.Empty;
+        }
+
+        public EnabledOtpAuthenticatorResponse(string secretKey)
+        {
+            SecretKey =secretKey;
+        }
+    }
+}
