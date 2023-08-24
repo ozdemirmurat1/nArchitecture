55db591ca1e499db5eaf0f7947389c3d1407dd61
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-08-24 13:19:31 +0300

    Commit

--- src/demoProjects/rentACar/Application/Features/Auths/Commands/Login/LoginCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/Login/LoginCommand.cs
@@ -7,11 +7,6 @@ using Core.Security.Entities;
 using Core.Security.Enums;
 using Core.Security.JWT;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Auths.Commands.Login
 {
---
+++ src/demoProjects/rentACar/Application/Features/Auths/Profiles/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Auths.Commands.RevokeToken;
+using AutoMapper;
+using Core.Security.Entities;
+
+namespace Application.Features.Auths.Profiles
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<RefreshToken, RevokedTokenResponse>().ReverseMap();
+        }
+    }
+}
