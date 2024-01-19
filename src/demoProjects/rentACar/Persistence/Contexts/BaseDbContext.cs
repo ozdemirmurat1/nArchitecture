4cafb9287714e19bbb8af895fc6dbe36e454a29b
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-01-19 22:29:59 +0300

    Commit

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -2,12 +2,7 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;  // SqlServer dan geliyor
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence.Contexts
 {
